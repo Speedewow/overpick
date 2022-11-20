801d0afe9b9144b077d18522a28435a80b87c518
Speede <Speedew0w@yandex.ru>
2022-11-20 20:03:22 +0300

    chore(stats): improove stats animation

--- src/components/analysis/stats/index.tsx
+++ src/components/analysis/stats/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
-import { useAppDispatch } from '../../../hooks';
+import cx from 'classnames';
+import { startSelector } from '../../../selectors';
+import { useAppDispatch, useAppSelector } from '../../../hooks';
 import { clearState } from '../../../slices/pickSlice';
 import { setEnd } from '../../../slices/startSlice';
 import { NEW, TITLE } from './constants';
@@ -8,6 +10,10 @@ import styles from './styles.module.scss';
 
 export const Stats = () => {
   const dispatch = useAppDispatch();
+  const start = useAppSelector(startSelector);
+
+  const titleClassName = cx(styles.title, !start && styles.titleHidden);
+  const buttonClassName = cx(styles.button, !start && styles.buttonHidden);
 
   const handleBack = () => {
     dispatch(clearState());
@@ -16,19 +22,21 @@ export const Stats = () => {
 
   return (
     <div className={styles.stats}>
-      <h2 className={styles.title}>{TITLE}</h2>
-      <div className={styles.container}>
-        <Stat name="RESULT" condition={30} />
-        <Stat name="WIN BEFORE 35 MIN" condition={20} />
-        <Stat name="WIN AFTER 35 MIN" condition={15} />
-        <Stat name="TEAM FIGHT POTENTIAL" condition={55} />
-        <Stat name="SOLO KILLS POTENTIAL" condition={75} />
-        <Stat name="SAVE POTENTIAL" condition={65} />
-        <Stat name="MOBILITY" condition={80} />
-        <Stat name="META" condition={90} />
-        <Stat name="MOBILITY" condition={95} />
-      </div>
-      <button className={styles.button} onClick={handleBack} aria-label={NEW}>
+      <h2 className={titleClassName}>{TITLE}</h2>
+      {start && (
+        <div className={styles.container}>
+          <Stat name="RESULT" condition={80} />
+          <Stat name="WIN BEFORE 35 MIN" condition={20} />
+          <Stat name="WIN AFTER 35 MIN" condition={15} />
+          <Stat name="TEAM FIGHT POTENTIAL" condition={55} />
+          <Stat name="SOLO KILLS POTENTIAL" condition={75} />
+          <Stat name="SAVE POTENTIAL" condition={65} />
+          <Stat name="MOBILITY" condition={80} />
+          <Stat name="META" condition={90} />
+          <Stat name="MOBILITY" condition={95} />
+        </div>
+      )}
+      <button className={buttonClassName} onClick={handleBack} aria-label={NEW}>
         {NEW}
       </button>
     </div>
